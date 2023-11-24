d997cc75501307f4dc2b062f5daf756cf871f734
acorriaz <mikey.swx@gmail.com>
2023-11-24 18:45:58 +0700

    Add button to toggle border

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-// create Grid
+const pickerColor = document.querySelector(".color-picker");
+const rainbowColor = document.querySelector(".color-picker");
+const bwColor = document.querySelector(".color-picker");
+const eraser = document.querySelector(".eraser");
+const clearGrid = document.querySelector(".clear");
+const border = document.querySelector(".border-toggle");
+const gridContainer = document.querySelector(".grid");
+const gridDimensionInput = document.querySelector(".dimension-input");
+const dimensionText = document.querySelector(".dimension");
+let grid;
+let isBorder = 1;
 
-const grid = document.querySelector(".grid");
+function changeDimensionText() {
+  dimensionText.textContent = `${gridDimensionInput.value} X ${gridDimensionInput.value}`;
+}
 
 function createGrid(dimension) {
   let container = document.createElement("div");
   container.className = "container";
-  grid.removeChild(grid.lastChild);
+  gridContainer.removeChild(gridContainer.lastChild);
   for (let i = 0; i < dimension; i++) {
     let parent = document.createElement("div");
     parent.className = "grid-parent";
@@ -13,9 +25,36 @@ function createGrid(dimension) {
     for (let j = 0; j < dimension; j++) {
       let child = document.createElement("div");
       child.className = "grid-box";
+      if (isBorder === 1) {
+        child.classList.add("border");
+      }
       parent.appendChild(child);
     }
     container.appendChild(parent);
   }
-  grid.appendChild(container);
+  gridContainer.appendChild(container);
+  grid = document.querySelectorAll(".grid-box");
 }
+
+function borderToggle() {
+  if (isBorder === 0) {
+    ++isBorder;
+    grid.forEach((element) => {
+      element.classList.add("border");
+    });
+  } else {
+    --isBorder;
+    grid.forEach((element) => {
+      element.classList.remove("border");
+    });
+  }
+}
+
+border.addEventListener("click", borderToggle);
+
+gridDimensionInput.addEventListener("input", () => {
+  changeDimensionText();
+  createGrid(gridDimensionInput.value);
+});
+
+window.onload = createGrid(16);
